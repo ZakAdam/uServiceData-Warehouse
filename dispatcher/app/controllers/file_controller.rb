bf3c7ec2f87c0309afe21d70159aae343d9444d5
Adam Žák <adam.hrac@gmail.com>
2021-12-17 15:59:36 +0100

    Add docker-compose (main)

--- dispatcher/app/controllers/file_controller.rb
+++ dispatcher/app/controllers/file_controller.rb
@@ -3,11 +3,11 @@ class FileController < ApplicationController
 
   def new
     if params[:file_type] == "GLS"
-      render json: {response: "\n\n\n\nGLS file successfully received!\n\n\n\n"}
+      render json: { response: "\n\n\n\nGLS file successfully received!\n\n\n\n" }
       return
     end
     puts "\n\nLets gooooooooooooooooooooooooooooooooooo\n\n"
-    render json: {response: "\n\n\n\nsicko zbehlo jako ma\n\n\n\n"}
+    render json: { response: "\n\n\n\nsicko zbehlo jako ma\n\n\n\n" }
   end
 
 end
