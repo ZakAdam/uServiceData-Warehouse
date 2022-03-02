f31f7787d427284d90210b83c95545886a49cd51
Adam Žák <adam.zak@softpoint.sk>
2022-03-02 18:00:43 +0100

    New upload scritp for testing (async_invoices)

--- Admin-Service/app/workers/new_invoice_upload.rb
+++ Admin-Service/app/workers/new_invoice_upload.rb
@@ -2,10 +2,17 @@ class NewInvoiceUpload
   include Sidekiq::Worker
   sidekiq_options queue: 'transport_invoices'
 
-  def perform(file)
+  def perform(file_name)
+    #puts "\n\n"
+    #result = RestClient.post "#{ENV.fetch("TRANSPORT_URL")}/gls_invoice/process", :file_type => 'gls', :file => file
+    #puts result
+    #puts 'Toto je vypis pri prilezitsti noveho uploadu :)'
+    #puts "\n\n"
+
     puts "\n\n"
-    result = RestClient.post "#{ENV.fetch("TRANSPORT_URL")}/gls_invoice/process", :file_type => 'gls', :file => file
-    puts result
+    #response = RestClient.get "#{ENV.fetch("FILE_URL")}/get_file", {params: {name: file_name}}
+    response = RestClient.get "admin_service:3000/get_file", {params: {name: file_name}}
+    puts response
     puts 'Toto je vypis pri prilezitsti noveho uploadu :)'
     puts "\n\n"
   end
