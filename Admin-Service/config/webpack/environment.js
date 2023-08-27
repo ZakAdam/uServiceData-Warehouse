57c822cecfc0c5b569b0700ee04e84c15408b348
Adam Žák <adam.zak@softpoint.sk>
2023-08-28 00:22:47 +0200

    Add fix for precompile bug (add-semantics-service-DP1)

--- Admin-Service/config/webpack/environment.js
+++ Admin-Service/config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// Needed to compile assets:precompile on older Ruby version
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
