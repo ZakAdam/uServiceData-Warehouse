7ee964c3502ec384c66b33e342b24fa651ba5115
Adam Žák <adam.zak@softpoint.sk>
2023-08-27 23:38:55 +0200

    Add changes from files (add-semantics-service-DP1)

--- semantics/xml_parser.rb
+++ semantics/xml_parser.rb
@@ -1,6 +1,10 @@
 # File for parsing content of xml files.
 # Parsed data are used to determine data origin using given headers/keys and ontology.
 require 'nokogiri'
+require 'rdf'
+require 'linkeddata'
+require 'rdf/ntriples'
+
 
 def load_keys
   doc = File.open(FILE) { |f| Nokogiri::XML(f) }
@@ -9,3 +13,25 @@ end
 
 FILE = '/home/adam/Desktop/FIIT/DP/files/Heureka/mensie.xml'
 puts load_keys
+
+# https://github.com/ruby-rdf/rdf
+RDF::Reader.open("https://ruby-rdf.github.io/rdf/etc/doap.nt") do |reader|
+  reader.each_statement do |statement|
+    puts statement.inspect
+  end
+end
+
+
+graph = RDF::Graph.load("https://ruby-rdf.github.io/rdf/etc/doap.nt")
+query = RDF::Query.new({
+                         person: {
+                           RDF.type  => RDF::Vocab::FOAF.Person,
+                           RDF::Vocab::FOAF.name => :name,
+                           RDF::Vocab::FOAF.mbox => :email,
+                         }
+                       }, **{})
+
+query.execute(graph) do |solution|
+  puts "name=#{solution.name} email=#{solution.email}"
+end
+
