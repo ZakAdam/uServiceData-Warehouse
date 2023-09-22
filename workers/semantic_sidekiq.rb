25f62d938e240e1e93597713cc956421818e0836
Adam Žák <adam.zak@softpoint.sk>
2023-09-22 16:33:09 +0200

    Rework to semantic file types (add-semantics-service-DP1)

---
+++ workers/semantic_sidekiq.rb
@@ -0,0 +1,33 @@
+require 'sidekiq'
+require 'sidekiq-cron'
+require 'rest-client'
+require 'dotenv'
+
+Dotenv.load
+
+sidekiq_config = { url: ENV['REDIS_SIDEKIQ_URL'] }
+
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_config
+end
+
+class SemanticProcessing
+  include Sidekiq::Worker
+  sidekiq_options queue: 'semantic_pipeline'
+  sidekiq_options :retry => 2
+
+  def perform(options, data)
+    puts 'In sidekiq'
+    puts options
+    puts data
+
+    #RestClient::Request.execute(method: :post, url: 'saver:3000/review/save',
+    #                            timeout: 600, headers: {params: {review_param: review_params, product_param: product_params, docker_id: self.jid}})
+
+    #RestClient.post "saver:3000/review/save", review_param: review_params, product_param: product_params, docker_id: self.jid
+  end
+end
