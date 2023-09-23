ec68ea9a975d52c04712d200151cffd98463e86d
Adam Žák <adam.zak@softpoint.sk>
2023-09-23 15:54:44 +0200

    Add charSet and changes to Admin (add-semantics-service-DP1)

--- Admin-Service/app/workers/new_invoice_upload.rb
+++ Admin-Service/app/workers/new_invoice_upload.rb
@@ -20,11 +20,14 @@ class NewInvoiceUpload
   sidekiq_options queue: 'transport_invoices'
   sidekiq_options :retry => 2
 
-  def perform(file_name)
+  def perform(file_name, file_type)
+    puts 'Halooooooooooooooooooooooo'
     begin
       #RestClient.get "admin_service:3000/get_file", {params: {name: file_name, jid: self.jid}}
       RestClient::Request.execute(method: :get, url: 'admin_service:3000/get_file',
-                                  timeout: 600, headers: {params: {name: file_name, jid: self.jid}})
+                                  timeout: 600, headers: { params: { name: file_name, file_type: file_type, jid: self.jid } })
+      #RestClient::Request.execute(method: :get, url: 'admin_service:3000/get_file',
+      #                            timeout: 600, headers: { params: { name: file_name, file_type: file_type, jid: self.jid } })
     rescue RestClient::Exceptions::ReadTimeout
       puts "\n\nDoslo k timeoutu!\n\n"
       puts self.jid
