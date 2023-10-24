a733b7b4c5b61cf12ae08ad28a8c06bd93777e5f
Adam Žák <adam.zak@softpoint.sk>
2023-10-24 15:54:50 +0200

    Add more nodes for more endpoints (add-semantics-service-DP1)

--- processors/optional_processors/optional_processor.rb
+++ processors/optional_processors/optional_processor.rb
@@ -19,31 +19,10 @@ post '/geo_location/keep_country' do
   RestClient.post url.to_s, file_type: params[:file_type], file: data, docker_id: @docker_id, jid: jid
 end
 
-post '/heureka_reviews/process' do
-  parser = Nori.new
-  parsed_reviews = if params.key?(:file)
-                     parser.parse(params['file'][:tempfile].read)
-                   else
-                     parser.parse(params['reviews'])
-                   end
+post '/notifications/send_email' do
+  puts 'Sending email notification to the assigned people...'
 
-  RestClient.post "saver:3000/heureka_reviews/save", :reviews => parsed_reviews, :docker_id => @docker_id
-end
+  # Query file_type by id, to get country?
 
-post '/dpd_invoice/process' do
-  RestClient.post "saver:3000/package_tracking/save", :trackings => data, :docker_id => @docker_id
-end
-
-post '/package_tracking/process' do
-  data = SmarterCSV.process(params['file'][:tempfile].path, {col_sep: ';'})
-
-  response = RestClient.get 'saver:3000/package_simple/last_id'
-  last_id = JSON.parse(response.body)['id']
-  response = RestClient.get 'saver:3000/package_simple/depots'
-  @depots = JSON.parse(response.body)
-
-  RestClient.post "saver:3000/package_simple/save", :new_trackings => new_trackings, :new_trackings_details => new_trackings_details, :new_consignee => new_consignee, :docker_id => @docker_id
-end
-
-post '/single_reviews/process' do
+  RestClient.post url.to_s, file_type: params[:file_type], file: data, docker_id: @docker_id, jid: jid
 end
