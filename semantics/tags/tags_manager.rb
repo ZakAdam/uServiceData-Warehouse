1567cbe21e5bb0eeac98ba0d42a97c203c7957f8
Adam Žák <adam.zak@softpoint.sk>
2024-03-13 15:17:43 +0100

    Add conditions fix and comments :) (add-semantics-service-DP1)

--- semantics/tags/tags_manager.rb
+++ semantics/tags/tags_manager.rb
@@ -47,18 +47,31 @@ def get_supplier_by_tags(file_ending, file_type, charset, language, headers)
   #query = "FT.SEARCH supplierIndex @tags:{binary|en}"
 
   # query = "FT.SEARCH supplierIndex @tags:{#{file_ending}|#{file_type.gsub('-', '\-')}|#{charset.gsub('-', '\-')}|#{language}|#{headers[0..4].join('|')}} RETURN 1 name WITHSCORES"
-  query = "FT.SEARCH supplierIndex @tags:{#{file_type.gsub(%r{[/\\\-.]}, 
-                                                           '\\\\\0')}}@tags:{#{file_ending}}@tags:{#{charset.gsub(%r{[/\\\-.]},
-                                                           '\\\\\0')}}@tags:{#{language}}@tags:{#{headers[0..4].join('|')}} RETURN 1 name"
+  # query = "FT.SEARCH supplierIndex @tags:{#{file_type.gsub(%r{[/\\\-.]}, 
+  #                                                         '\\\\\0')}}@tags:{#{file_ending}}@tags:{#{charset.gsub(%r{[/\\\-.]},
+  #                                                         '\\\\\0')}}@tags:{#{language}}@tags:{#{headers[0..6].join('|')}} RETURN 1 name"
+
+  query = "FT.SEARCH supplierIndex @tags:{#{file_type.gsub(%r{[/\\\-.]},
+           '\\\\\0')}|#{file_ending}|#{charset.gsub(%r{[/\\\-.]},
+           '\\\\\0')}|#{language}|#{headers[0..4].join('|')}} WITHSCORES"
+
+  puts query
+  print query
 
   results = REDIS.call(query.split)
 
-  puts "Tags identified supplier: #{results[2][1]}"
-  results[2][1].downcase
+  puts results
+
+  # puts "Tags identified supplier: #{results[2][1]}"
+  puts "Tags identified supplier: #{results[3][1]}"
+  # results[2][1].downcase
+  results[3][1].downcase
 end
 
 def get_path_by_tags(supplier, conditions)
-  query = "MGET #{conditions.join(' ')} #{supplier}"
+  conditions = conditions.join(' ') unless conditions.empty?
+
+  query = "MGET #{conditions} #{supplier}"
 
   path = REDIS.call(query.split).compact
   puts "Path created by tags: #{path}"
